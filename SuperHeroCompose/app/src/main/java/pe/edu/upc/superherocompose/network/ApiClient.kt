d59c3d05eaede5f44502749c37ed3549f778433d
jmayta1984 <jmayta@pucp.edu.pe>
2024-04-08 21:45:12 -0500

    First commit

--- SuperHeroCompose/app/src/main/java/pe/edu/upc/superherocompose/network/ApiClient.kt
+++ SuperHeroCompose/app/src/main/java/pe/edu/upc/superherocompose/network/ApiClient.kt
@@ -4,5 +4,5 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    const val BASE_URL = "https://www.superheroapi.com/api.php/";
+    const val BASE_URL = "https://www.superheroapi.com/api.php/"
 }
