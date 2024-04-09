3f339bf998f2256e0fbe80dd9a00ee29884a678b
jmayta1984 <jmayta@pucp.edu.pe>
2024-04-08 21:34:49 -0500

    First commit

--- SuperHeroCompose/app/src/main/java/pe/edu/upc/superherocompose/ui/home/Home.kt
+++ SuperHeroCompose/app/src/main/java/pe/edu/upc/superherocompose/ui/home/Home.kt
@@ -7,7 +7,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,6 +23,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.skydoves.landscapist.ImageOptions
 import com.skydoves.landscapist.glide.GlideImage
@@ -29,33 +37,56 @@ fun Home() {
     val name = remember {
         mutableStateOf("")
     }
+
+    val heroes = remember {
+        mutableStateOf(emptyList<Hero>())
+    }
+
     Scaffold { paddingValues ->
         Column(modifier = Modifier.padding(paddingValues)) {
-            HeroSearch(name)
-            HeroList(name)
+            HeroSearch(name, heroes)
+            HeroList(heroes)
         }
     }
 
 }
 
 @Composable
-fun HeroSearch(name: MutableState<String>) {
+fun HeroSearch(name: MutableState<String>, heroes: MutableState<List<Hero>>) {
+    val heroRepository = HeroRepository()
 
+    OutlinedTextField(
+        value = name.value,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(2.dp),
+        onValueChange = {
+            name.value = it
+        },
+        placeholder = { Text("Search hero") },
+        leadingIcon = {
+            Icon(Icons.Filled.Search, "Search hero")
+        },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text,
+            imeAction = ImeAction.Search
+        ),
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                heroRepository.searchHero(name.value) {
+                    heroes.value = it
+                }
+            }
+        )
+
+    )
 }
 
 
 @Composable
-fun HeroList(name: MutableState<String>) {
-    val heroes = remember {
-        mutableStateOf(emptyList<Hero>())
-    }
-    val heroRepository = HeroRepository()
+fun HeroList(heroes: MutableState<List<Hero>>) {
 
-    heroRepository.searchHero(name.value) {
-        heroes.value = it
-    }
     LazyColumn {
-
 
         items(heroes.value) { hero ->
             HeroCard(hero)
@@ -65,12 +96,14 @@ fun HeroList(name: MutableState<String>) {
 
 @Composable
 fun HeroCard(hero: Hero) {
-    Card (modifier = Modifier
-        .fillMaxWidth()
-        .padding(4.dp)) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp)
+    ) {
         Row {
             HeroImage(hero.image.url)
-            Column (modifier = Modifier.padding(4.dp)){
+            Column(modifier = Modifier.padding(4.dp)) {
                 Text(text = hero.name, fontWeight = FontWeight.Bold)
                 Text(text = hero.biography.fullName)
             }
